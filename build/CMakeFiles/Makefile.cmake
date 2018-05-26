3041c2d8b6351a5c07239bc17e1e1f19bd344909
thetrippleh <henrik.leisdon@uni-weimar.de>
2018-05-26 11:46:37 +0200

    complete task 3.2

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,5 +43,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "source/CMakeFiles/example.dir/DependInfo.cmake"
+  "source/CMakeFiles/aufgabe_2_und_3.dir/DependInfo.cmake"
   )
--- source/aufgabe_2_und_3.cpp
+++ source/aufgabe_2_und_3.cpp
@@ -1,18 +1,62 @@
 #include <list>
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <set>
+
 
 int main()
 {
     std::list<unsigned int> liste;
     for(int i=0;i<=100;i++){
-        liste.push_back(rand()); 
+        liste.push_back(rand()%101); 
     }
 
     for (std::list<unsigned int>::iterator it = liste.begin(); it != liste.end(); it++){
-    std::cout << *it;   
-
+        std::cout << *it <<" ";   
     }
 
-    return 0;
-}
+
+
+ std::set<unsigned int> intcount;
+ int platzhalter = 0;
+     
+
+    for(std::list<unsigned int>::iterator it = liste.begin(); it!=liste.end();it++){
+       auto search = intcount.find(*it);
+       if(search != intcount.end() ){
+           platzhalter+=1;
+       }else{
+           intcount.insert(*it);
+       }
+    
+    }
+   std::cout <<"anzahl:" << intcount.size() << "\n" << "test: " << platzhalter << "\n";
+
+
+
+
+
+//Nicht vorkommende Zahl:
+
+    std::set<unsigned int> notIncluded;
+    for(unsigned int i = 0; i<=100; i++){
+        notIncluded.insert(i);
+    }
+
+     for(std::list<unsigned int>::iterator it = liste.begin(); it!=liste.end();it++){
+       notIncluded.erase(*it);
+         
+     }
+    if(notIncluded.size()==0){
+        std::cout << "alle Zahlen enthalten" << "\n";
+    }else{
+        for(std::set<unsigned int>::iterator it = notIncluded.begin(); it!=notIncluded.end();it++){     
+        std::cout << *it << "\n";
+        }
+    }
+    
+    
+    
+} 
+    
