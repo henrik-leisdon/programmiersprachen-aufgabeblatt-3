de98560626213d3b97e5505c32c6f295720d0b85
henrik-leisdon <33347624+henrik-leisdon@users.noreply.github.com>
2018-05-27 13:47:45 +0200

    changes in task 9

--- source/aufgabe_9.cpp
+++ source/aufgabe_9.cpp
@@ -1,39 +1,35 @@
-# include <cstdlib>
-# include <vector>
-#include <list>
-#include <iostream>
-#include <iterator>
-#include <algorithm>
+#include <cstdlib> // std::rand()
+#include <vector> // std::vector<>
+#include <list> // std::list<>
+#include <iostream> // std::cout
+#include <iterator> // std::ostream_iterator<>
+#include <algorithm> // std::reverse, std::generate
 
-/*std :: rand ()
-std :: vector < >
-std :: list < >
-std :: cout
-std :: ostream_iterator < >
-std :: reverse , std :: generate
-*/
-
-int main ()
+int main()
 {
-std::vector<int>v_0(10);
-for ( auto & v : v_0 ) {
-v = std::rand ();
-}
-std::copy(std::cbegin( v_0 ), std::cend( v_0 ),
-std::ostream_iterator < int >( std :: cout, "\n"  ));
+std::vector<int> v_0(10);
 
-std::list<int> l_0(std::cbegin(v_0) , std::cend(v_0));  
-std::list<int> l_1(std::cbegin(l_0), std::cend(l_0)); 
-std::reverse(std::begin( l_1 ),std::end( l_1 ));
-std::copy(std::cbegin ( l_1 ) , std :: cend ( l_1 ) ,
-std::ostream_iterator<int>(std::cout, "\n" ));
+for (auto& v : v_0) {
+v = std::rand();
+}
+
+std::copy(std::cbegin(v_0), std::cend(v_0),
+std::ostream_iterator<int>(std::cout, "\n"));
+
+std::list<int> l_0(std::cbegin(v_0), std::cend(v_0));
+std::list<int> l_1(std::cbegin(l_0), std::cend(l_0));
+std::reverse(std::begin(l_1), std::end(l_1));
+std::copy(std::cbegin(l_1), std::cend(l_1),
+std::ostream_iterator<int>(std::cout, "\n"));
 
 l_1.sort();
-std::copy(l_1.cbegin(), l_1.cend() ,
+std::copy(l_1.cbegin(), l_1.cend(),
 std::ostream_iterator<int>(std::cout, "\n"));
 
 std::generate(std::begin(v_0), std::end(v_0), std::rand);
 std::copy(v_0.crbegin(), v_0.crend(),
-std::ostream_iterator<int>(std::cout,"\n"));
+std::ostream_iterator<int>(std::cout, "\n"));
+
 return 0;
+
 }
